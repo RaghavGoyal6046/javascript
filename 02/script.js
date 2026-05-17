9944559c6dce08fdc4ac6da7aac3daead2d71916
Raghav Goyal <raghavgoyal6046@gmail.com>
2026-05-18 00:40:30 +0530

    Day 1.2

--- 02/script.js
+++ 02/script.js
@@ -2,7 +2,19 @@
 
 // var
 var age=22;
+age ="Raghav";
 var num1=10;
 var num2=20;
 
 console.log(age);
+
+
+// let
+let city="Delhi";
+city="Mumbai";
+console.log(city);
+
+// const
+const country="India";
+// country="USA"; // This will throw an error because we cannot reassign a value to a constant variable.
+console.log(country);       

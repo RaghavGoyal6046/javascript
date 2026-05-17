24bff899dde0dd1cee3761ff11192e113e19da10
Raghav Goyal <raghavgoyal6046@gmail.com>
2026-05-18 00:19:47 +0530

    Day 1

---
+++ 01/script.js
@@ -0,0 +1,2 @@
+console.log("Hello from external JavaScript file!");
+console.log("This is a simple JavaScript file that is linked to an HTML document.");
---
+++ script.js
@@ -0,0 +1,8 @@
+// Variables
+
+// var
+var age=22;
+var num1=10;
+var num2=20;
+
+console.log(age);

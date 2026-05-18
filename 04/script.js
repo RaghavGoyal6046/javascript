4cddb7fca0f390bf857f82f0c9c6cd41757eae31
Raghav Goyal <raghavgoyal6046@gmail.com>
2026-05-18 18:42:47 +0530

    Modification

---
+++ 04/script.js
@@ -0,0 +1,11 @@
+// Conditional Statements in JavaScript
+
+const age = 18;
+
+if (age < 18) {
+    console.log("You are a minor.");
+} else if (age === 18) {
+    console.log("Congratulations on reaching adulthood!");
+} else {
+    console.log("You are an adult.");
+}

44ca19d36a4862253d76c3bafa6f2cfc3248c476
Raghav Goyal <raghavgoyal6046@gmail.com>
2026-05-18 18:52:09 +0530

    Conditionnal Statement in JS

--- 04/script.js
+++ 04/script.js
@@ -1,5 +1,6 @@
 // Conditional Statements in JavaScript
 
+// ladder if-else statement
 const age = 18;
 
 if (age < 18) {
@@ -9,3 +10,36 @@ if (age < 18) {
 } else {
     console.log("You are an adult.");
 }
+// ternary operator
+const isAdult = age >= 18 ? "Yes, you are an adult." : "No, you are a minor.";
+console.log(isAdult);
+
+// switch statement
+const dayOfWeek = 3;
+
+switch (dayOfWeek) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day of the week.");
+}       
+
